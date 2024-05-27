a4929b80f09f2a7ba2e69afd16ab992d615b40ef
Guilherme Caramori <gcaramori16@gmail.com>
2024-05-27 09:54:37 -0300

    classes to packages changes

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -42,7 +42,7 @@ export default function Services() {
 
         <ComponentTransition>
           <div className="block relative my-auto overflow-hidden rounded-lg">
-            <Image src="/services.jpg" alt="gym" width={600} height={600} />
+            <Image src="/services.jpeg" alt="gym" width={600} height={600} />
           </div>
         </ComponentTransition>
       </div>
